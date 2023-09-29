acf4a140a974fd7b9bc67f4b5f6a71c5dfb7aa7a
Carlor Jr Diaz <cjunior90@gmail.com>
2023-09-28 19:19:55 -0500

    Set up DB Configration and Creation

---
+++ ShoppingAPI_Jueves_2023II/DAL/Entities/Country.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingAPI_Jueves_2023II.DAL.Entities
+{
+    public class Country : AuditBase
+    {
+        [Display(Name = "País")] // Para identificar el nombre más fácil
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")] // Longitud máx
+        [Required(ErrorMessage = "Es campo {0} es obligatorio")] // Campo obligatorio
+        public string Name { get; set; }
+    }
+}
